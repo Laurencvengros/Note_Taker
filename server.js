66704bee55fb73c29bbeb17d7dec5b0ab1b28daf
Lauren <l.cvengros@icloud.com>
2022-09-27 22:07:33 -0400

    switched order of get commands to make notes run

--- server.js
+++ server.js
@@ -14,19 +14,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 const writeToFile = (destination, noteContent) =>
-fs.writeFile(destination, JSON.stringify(noteContent), (err) =>{
-    if (err){
-        console.error(err)
-    } else {
-        console.info(`\nData written to ${destination}`)
-    }
-})
+    fs.writeFile(destination, JSON.stringify(noteContent), (err) => {
+        if (err) {
+            console.error(err)
+        } else {
+            console.info(`\nData written to ${destination}`)
+        }
+    })
 
-const readAndAppend = (noteContent, file) =>{
-    fs.readFile(file, 'utf8', (err, data) =>{
-        if(err){
+const readAndAppend = (noteContent, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+        if (err) {
             console.error(err);
-        } else{
+        } else {
             const noteData = JSON.parse(data);
             noteData.push(noteContent);
             writeToFile(file, noteData);
@@ -36,20 +36,20 @@ const readAndAppend = (noteContent, file) =>{
 
 
 app.get("/api/notes", (req, res) => {
-    fs.readFile(path.join(__dirname, "./db/db.json"),"utf8", (error,notes) => {
+    fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (error, notes) => {
         if (error) {
             return console.log("cound not get note")
         }
-        
+
         res.json(JSON.parse(notes))
     })
-  });
-  
-app.post("/api/notes", (req, res) =>{
-  
-    const {date, title, text} = req.body
+});
 
-    if(req.body){
+app.post("/api/notes", (req, res) => {
+
+    const { date, title, text } = req.body
+
+    if (req.body) {
         const newNote = {
             date,
             title,
@@ -63,17 +63,19 @@ app.post("/api/notes", (req, res) =>{
     }
 });
 
+//route for notes page
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
 //route for index.html
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'))
-  });
-  
-  //route for notes page
-  app.get("/notes",(req, res) => {
-      res.sendFile(path.join(__dirname, "./public/notes.html"));
-  });
-  
+});
+
+
+
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
